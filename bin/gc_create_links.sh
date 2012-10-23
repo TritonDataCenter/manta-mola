433e435a85a951ba9a74c5addad061f23f233510
Nate Fitch <nate.fitch@joyent.com>
2012-10-23 22:22:43 +0000

    [MANTA-196] GC create links env set up.

--- bin/gc_create_links.sh
+++ bin/gc_create_links.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-: ${MANTA_URL?"MANTA_URL must be set"}
+PATH=/opt/smartdc/mola/node_modules/manta/bin:$PATH
+[ -z $MANTA_KEY_ID ] && MANTA_KEY_ID=$(ssh-keygen -l -f ~/.ssh/id_rsa.pub | awk '{print $2}')
+[ -z $MANTA_URL ] && MANTA_URL=$(mdata-get manta_url)
+[ -z $MANTA_USER ] && MANTA_USER=poseidon
 
 for file in `mls /manta_gc/all/do | json -a name`; do
     mget /manta_gc/all/do/$file | bash && mrm /manta_gc/all/do/$file;
