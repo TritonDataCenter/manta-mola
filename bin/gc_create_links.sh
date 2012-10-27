944e45144e4cedf35dd67a938d04e5fc7331600f
Nate Fitch <nate.fitch@joyent.com>
2012-10-27 02:59:52 +0000

    [MANTA-196] Making gc_create_links.sh more robust.

--- bin/gc_create_links.sh
+++ bin/gc_create_links.sh
@@ -1,13 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
 PATH=/opt/smartdc/mola/node_modules/manta/bin:$PATH
 [ -z $MANTA_KEY_ID ] && MANTA_KEY_ID=$(ssh-keygen -l -f ~/.ssh/id_rsa.pub | awk '{print $2}')
 [ -z $MANTA_URL ] && MANTA_URL=$(mdata-get manta_url)
 [ -z $MANTA_USER ] && MANTA_USER=poseidon
 
-for file in `mls /$MANTA_USER/stor/manta_gc/all/do | json -a name`; do
-    mget /$MANTA_USER/stor/manta_gc/all/do/$file | bash && \
-	mrm /$MANTA_USER/stor/manta_gc/all/do/$file;
+MDIR=/$MANTA_USER/stor/manta_gc/all/do
+NOW=`date`
+
+function fatal {
+    echo "$NOW: $(basename $0): fatal error: $*" >&2
+    exit 1
+}
+
+function log {
+    echo "$NOW: $(basename $0): info: $*" >&2
+}
+
+log "listing $MDIR"
+for json in `mls $MDIR`
+do
+    FILE=`echo $json | json -a name`
+    MFILE=$MDIR/$FILE
+    mget $MFILE | bash
+    [[ $? -eq 0 ]] || fatal "Couldnt execute $MFILE"
+
+    mrm $MFILE
+    [[ $? -eq 0 ]] || fatal "Couldnt rm $MFILE"
+
+    log "processed $MFILE successfully"
 done
+[[ $? -eq 0 ]] || fatal "Couldnt list $MDIR"
 
 exit 0;
