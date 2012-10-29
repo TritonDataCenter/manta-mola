ead38dcc6be5fce926d4297cefc95b02d8cb268f
Nate Fitch <nate.fitch@joyent.com>
2012-10-29 16:21:13 +0000

    [MANTA-196] Adjusting path and vars for link creation.

--- bin/gc_create_links.sh
+++ bin/gc_create_links.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-PATH=/opt/smartdc/mola/node_modules/manta/bin:$PATH
-[ -z $MANTA_KEY_ID ] && MANTA_KEY_ID=$(ssh-keygen -l -f ~/.ssh/id_rsa.pub | awk '{print $2}')
-[ -z $MANTA_URL ] && MANTA_URL=$(mdata-get manta_url)
-[ -z $MANTA_USER ] && MANTA_USER=poseidon
+export PATH=/opt/smartdc/mola/build/node/bin:/opt/smartdc/mola/node_modules/manta/bin:$PATH
+[ -z $MANTA_KEY_ID ] && export MANTA_KEY_ID=$(ssh-keygen -l -f ~/.ssh/id_rsa.pub | awk '{print $2}')
+[ -z $MANTA_URL ] && export MANTA_URL=$(mdata-get manta_url)
+[ -z $MANTA_USER ] && export MANTA_USER=poseidon
 
 MDIR=/$MANTA_USER/stor/manta_gc/all/do
 NOW=`date`
 
 function fatal {
-    echo "$NOW: $(basename $0): fatal error: $*" >&2
+        echo "$NOW: $(basename $0): fatal error: $*" >&2
     exit 1
 }
 
 function log {
     echo "$NOW: $(basename $0): info: $*" >&2
-}
+    }
+
+COUNT=0
 
 log "listing $MDIR"
 for json in `mls $MDIR`
@@ -29,7 +31,10 @@ do
     [[ $? -eq 0 ]] || fatal "Couldnt rm $MFILE"
 
     log "processed $MFILE successfully"
+    ((COUNT++))
 done
+
 [[ $? -eq 0 ]] || fatal "Couldnt list $MDIR"
 
+log "done, processed $COUNT files"
 exit 0;
